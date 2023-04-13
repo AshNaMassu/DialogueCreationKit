b1be04b09d526a8040957cebdccb800596329191
Konstantin Vorobyev <konstantin.31.12@yandex.ru>
2023-04-13 21:29:16 +0300

    Запустил тестовую страницу с таблицей бд

---
+++ DialogueCreationKit/Designer/Components/TableNode.razor.cs
@@ -0,0 +1,11 @@
+﻿using DialogueCreationKit.Designer.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DialogueCreationKit.Designer.Components
+{
+    public partial class TableNode
+    {
+        [Parameter]
+        public Table Node { get; set; }
+    }
+}
