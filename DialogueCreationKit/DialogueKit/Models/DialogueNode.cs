fe274ce9e5a52e012fe71880a4adad8066b69e59
Konstantin Vorobyev <konstantin.31.12@yandex.ru>
2023-05-13 20:30:36 +0300

    убрал IsActive

--- DialogueCreationKit/DialogueKit/Models/DialogueNode.cs
+++ DialogueCreationKit/DialogueKit/Models/DialogueNode.cs
@@ -7,7 +7,7 @@ namespace DialogueCreationKit.DialogueKit.Models
     {
         public Guid Id { get; set; }
         public DialogueStage Stage { get; set; }
-        public bool IsActive { get; set; }
+        //public bool IsActive { get; set; }
         public string Message { get; set; }
         //public Guid MessageId { get; set; }
         public Guid? Child { get; set; }
@@ -17,7 +17,7 @@ namespace DialogueCreationKit.DialogueKit.Models
         {
             Id = Guid.NewGuid();
             Stage = DialogueStage.None;
-            IsActive = true;
+            //IsActive = true;
             Message = string.Empty;
         }
     }
